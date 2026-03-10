6a036acb16ec86604ac5d5e90589462a435662f6
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-03-10 11:21:47 +0700

    feat: menambahkan input fungsi dan ukuran array

--- insort.cpp
+++ insort.cpp
@@ -7,3 +7,12 @@ int n;
 int main() {
     return 0;
 }
+void input (){
+    while (true){
+        cout << "Masukkan jumlah data Pada Array : ";
+        cin >> n;
+        if (n <= 20) break;
+        else cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+    }
+    // ... (input data loop)
+}
